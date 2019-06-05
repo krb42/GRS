7333d28618ad6506fb224124a9acfd45df35d90e
Kevin Firth <krfirth@gmail.com>
2019-06-05 21:44:59 +0800

    Update scripts to be executed

---
+++ src/lib/GRS_DBUP/GRS_DBUP/Scripts/GRS002 Insert Base Seed Data.sql
@@ -0,0 +1,41 @@
+﻿/*------------------------------------------------------------------------------------
+   Project/Ticket#: GRS
+   <Description>    Insert Base Seed Data for GRS tables - GRS_Meeting and GRS_User  </Description>
+   <Version>        1.0.0                         </version>
+-------------------------------------------------------------------------------------*/
+
+USE [$DatabaseName$]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+IF NOT EXISTS (SELECT * FROM GRS_User WHERE UserName = 'Admin')
+BEGIN
+    SET IDENTITY_INSERT dbo.GRS_User ON
+    INSERT INTO GRS_User ([UserID], [UserName], [UserCode], [AccessCode], 
+                            [Deleted], [TSCreateDate], [TSCreateUser],       [VersionAutoID])
+                    SELECT 1,       'Admin',    'ADMIN',    'snafu',      
+                            0,         GETDATE(),      'GRS Basic Seed Data', NEXT VALUE FOR VersionAutoID_Sequence;
+    SET IDENTITY_INSERT dbo.GRS_User OFF
+END
+
+
+IF NOT EXISTS (SELECT * FROM GRS_Meeting WHERE MeetingID = 1)
+BEGIN
+    SET IDENTITY_INSERT dbo.GRS_Meeting ON
+    INSERT INTO GRS_Meeting ([MeetingID], [Description],   [Name],               [ReportTitle],                [StartDate], [EndDate], 
+                                [Deleted], [TSCreateDate], [TSCreateUser],        [VersionAutoID])
+                    SELECT    1,          'Admin Meeting', 'Admin Name Meeting', 'Admin Meeting Report Tiele', GETDATE(),   GETDATE(), 
+                                0,         GETDATE(),      'GRS Basic Seed Data', NEXT VALUE FOR VersionAutoID_Sequence;
+    SET IDENTITY_INSERT dbo.GRS_Meeting OFF
+END
+
+
+
+
+
--- src/lib/GRS_DBUP/GRS_DBUP/Scripts/ready GRS000B Create Sport Tables.sql
+++ src/lib/GRS_DBUP/GRS_DBUP/Scripts/ready GRS000B Create Sport Tables.sql
@@ -1,6 +1,6 @@
 ﻿/*------------------------------------------------------------------------------------
    Project/Ticket#: GRS
-   <Description>    Create GRS tables             </Description>
+   <Description>    Create GRS tables - GRS_Sport, GRS_Sport_Statistics, GRS_Division, GRS_Event, GRS_Result and GRS_PlaceGetter </Description>
    <Version>        1.0.0                         </version>
 -------------------------------------------------------------------------------------*/
 
@@ -13,102 +13,6 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'GRS_User')
-BEGIN
-    CREATE TABLE [dbo].[GRS_User](
-        [UserID] [int] IDENTITY(1,1) NOT NULL,
-        [UserName] [nvarchar](80) NOT NULL,
-        [UserCode] [nvarchar](80) NOT NULL,
-
-        Deleted bit null,
-        VersionAutoID int null,
-
-        TSCreateDate datetime null,
-        TSCreateUser [nvarchar](80) NOT NULL,
-        TSModifyDate datetime null,
-        TSModifyUser [nvarchar](80) NULL,
-                
-        CONSTRAINT [PK_GRS_User_UserID] PRIMARY KEY CLUSTERED 
-            (
-                [UserID] ASC
-            ) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, 
-                        IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-    ) ON [PRIMARY]
-END
-GO
-
-
-IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'GRS_Entity')
-BEGIN
-
-    CREATE TABLE [dbo].[GRS_Entity](
-        [EntityID] [int] IDENTITY(1,1) NOT NULL,
-        [EntityCode] [nvarchar](10) NOT NULL,
-        [EntityName] [nvarchar](50) NOT NULL,
-        [MeetingID] [int] NOT NULL,
-
-        HandicapWeight float null,
-        [Population] int null,
-        Distance int null,
-
-        Deleted bit null,
-        VersionAutoID int null,
-        TSCreateDate datetime null,
-        TSCreateUser [nvarchar](80) NOT NULL,
-        TSModifyDate datetime null,
-        TSModifyUser [nvarchar](80) NULL,
-
-        CONSTRAINT [PK_GRS_Entity_EntityID] PRIMARY KEY CLUSTERED 
-            (
-                [EntityID] ASC
-            ) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, 
-                        IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-    ) ON [PRIMARY]
-END
-GO
-
-IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'GRS_Entity_Statistics')
-BEGIN
-
-    CREATE TABLE [dbo].[GRS_Entity_Statistic](
-        [EntityStatisticID] [int] IDENTITY(1,1) NOT NULL,
-        [EntityID] [int] NOT NULL,
-
-        Stat_LastResult_VersionAutoID int null,
-
-        Stat_Nominations int null,
-        Stat_Gold int null,
-        Stat_Silver int null,
-        Stat_Bronze int null,
-        Stat_HCapGold float null,
-        Stat_HCapSilver float null,
-        Stat_HCapBronze float null,
-
-        VersionAutoID int null,
-        TSCreateDate datetime null,
-        TSCreateUser [nvarchar](80) NOT NULL,
-        TSModifyDate datetime null,
-        TSModifyUser [nvarchar](80) NULL,
-
-        CONSTRAINT [PK_GRS_Entity_Statistic_EntityStatisticID] PRIMARY KEY CLUSTERED 
-            (
-                [EntityStatisticID] ASC
-            ) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, 
-                        IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-    ) ON [PRIMARY]
-END
-GO
-
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE CONSTRAINT_NAME = N'FK_GRS_Entity_Statistics_GRS_Entity')
-BEGIN
-    ALTER TABLE [dbo].[GRS_Entity_Statistic] WITH CHECK
-        ADD CONSTRAINT [FK_GRS_Entity_Statistic_GRS_Entity] FOREIGN KEY([EntityID])
-            REFERENCES [dbo].[GRS_Entity] ([EntityID])
-    ALTER TABLE [dbo].[GRS_Entity_Statistic] CHECK CONSTRAINT [FK_GRS_Entity_Statistic_GRS_Entity]
-END
-GO
-
-
 IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'GRS_Sport')
 BEGIN
 
@@ -119,7 +23,8 @@ BEGIN
         [MeetingID] [int] NOT NULL,
 
         Deleted bit null,
-        VersionAutoID int null,
+        VersionAutoID bigint null,
+
         TSCreateDate datetime null,
         TSCreateUser [nvarchar](80) NOT NULL,
         TSModifyDate datetime null,
@@ -147,7 +52,8 @@ BEGIN
         Stat_Nominations int null,
         Stat_Participants int null,
 
-        VersionAutoID int null,
+        VersionAutoID bigint null,
+
         TSCreateDate datetime null,
         TSCreateUser [nvarchar](80) NOT NULL,
         TSModifyDate datetime null,
@@ -172,120 +78,6 @@ END
 GO
 
 
-IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'GRS_Attribute')
-BEGIN
-
-    CREATE TABLE [dbo].[GRS_Attribute](
-        [AttributeID] [int] IDENTITY(1,1) NOT NULL,
-        [AttributeType] [nvarchar](50) NOT NULL,
-        [AttributeName] [nvarchar](50) NOT NULL,
-        [AttributeSetting] [nvarchar](1000) NULL,
-
-        ParentID int null,
-
-        Deleted bit null,
-        VersionAutoID int null,
-        TSCreateDate datetime null,
-        TSCreateUser [nvarchar](80) NOT NULL,
-        TSModifyDate datetime null,
-        TSModifyUser [nvarchar](80) NULL,
-                
-        CONSTRAINT [PK_GRS_Attribute_AttributeID] PRIMARY KEY CLUSTERED 
-            (
-                [AttributeID] ASC
-            ) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, 
-                        IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-    ) ON [PRIMARY]
-END
-GO		
-
-
-IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'GRS_Entity_Attribute')
-BEGIN
-
-    CREATE TABLE [dbo].[GRS_Entity_Attribute](
-        [EntityAttributeID] [int] IDENTITY(1,1) NOT NULL,
-        [EntityID] int NOT NULL,
-        [AttributeID] int NOT NULL,
-        [AttributeName] [nvarchar](50) NOT NULL,
-        [AttributeSetting] [nvarchar](1000) NULL,
-
-        VersionAutoID int null,
-        TSCreateDate datetime null,
-        TSCreateUser [nvarchar](80) NOT NULL,
-        TSModifyDate datetime null,
-        TSModifyUser [nvarchar](80) NULL,
-                
-        CONSTRAINT [PK_GRS_Entity_Attribute_EntityAttributeID] PRIMARY KEY CLUSTERED 
-            (
-                [EntityAttributeID] ASC
-            ) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, 
-                        IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-    ) ON [PRIMARY]
-END
-GO
-
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE CONSTRAINT_NAME = N'FK_GRS_Entity_Attribute_GRS_Attribute')
-BEGIN
-    ALTER TABLE [dbo].[GRS_Entity_Attribute] WITH CHECK
-        ADD CONSTRAINT [FK_GRS_Entity_Attribute_GRS_Attribute] FOREIGN KEY([AttributeID])
-            REFERENCES [dbo].[GRS_Attribute] ([AttributeID])
-    ALTER TABLE [dbo].[GRS_Entity_Attribute] CHECK CONSTRAINT [FK_GRS_Entity_Attribute_GRS_Attribute]
-END
-GO
-
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE CONSTRAINT_NAME = N'FK_GRS_Entity_Attribute_GRS_Entity')
-BEGIN
-    ALTER TABLE [dbo].[GRS_Entity_Attribute] WITH CHECK
-        ADD CONSTRAINT [FK_GRS_Entity_Attribute_GRS_Entity] FOREIGN KEY([EntityID])
-            REFERENCES [dbo].[GRS_Entity] ([EntityID])
-    ALTER TABLE [dbo].[GRS_Entity_Attribute] CHECK CONSTRAINT [FK_GRS_Entity_Attribute_GRS_Entity]
-END
-GO
-
-IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'GRS_Sport_Attribute')
-BEGIN
-
-    CREATE TABLE [dbo].[GRS_Sport_Attribute](
-        [SportAttributeID] [int] IDENTITY(1,1) NOT NULL,
-        [SportID] int NOT NULL,
-        [AttributeID] int NOT NULL,
-        [AttributeName] [nvarchar](50) NOT NULL,
-        [AttributeSetting] [nvarchar](1000) NULL,
-
-        VersionAutoID int null,
-        TSCreateDate datetime null,
-        TSCreateUser [nvarchar](80) NOT NULL,
-        TSModifyDate datetime null,
-        TSModifyUser [nvarchar](80) NULL,
-                
-        CONSTRAINT [PK_GRS_Sport_Attribute_SportAttributeID] PRIMARY KEY CLUSTERED 
-            (
-                [SportAttributeID] ASC
-            ) WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, 
-                        IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-    ) ON [PRIMARY]
-END
-GO
-
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE CONSTRAINT_NAME = N'FK_GRS_Sport_Attribute_GRS_Attribute')
-BEGIN
-    ALTER TABLE [dbo].[GRS_Sport_Attribute] WITH CHECK
-        ADD CONSTRAINT [FK_GRS_Sport_Attribute_GRS_Attribute] FOREIGN KEY([AttributeID])
-            REFERENCES [dbo].[GRS_Attribute] ([AttributeID])
-    ALTER TABLE [dbo].[GRS_Sport_Attribute] CHECK CONSTRAINT [FK_GRS_Sport_Attribute_GRS_Attribute]
-END
-GO
-
-IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS WHERE CONSTRAINT_NAME = N'FK_GRS_Sport_Attribute_GRS_Sport')
-BEGIN
-    ALTER TABLE [dbo].[GRS_Sport_Attribute] WITH CHECK
-        ADD CONSTRAINT [FK_GRS_Sport_Attribute_GRS_Sport] FOREIGN KEY([SportID])
-            REFERENCES [dbo].[GRS_Sport] ([SportID])
-    ALTER TABLE [dbo].[GRS_Sport_Attribute] CHECK CONSTRAINT [FK_GRS_Sport_Attribute_GRS_Sport]
-END
-GO
-
 IF NOT EXISTS(SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'GRS_Division')
 BEGIN
 
@@ -296,7 +88,8 @@ BEGIN
         [MeetingID] [int] NOT NULL,
 
         Deleted bit null,
-        VersionAutoID int null,
+        VersionAutoID bigint null,
+
         TSCreateDate datetime null,
         TSCreateUser [nvarchar](80) NOT NULL,
         TSModifyDate datetime null,
@@ -321,7 +114,8 @@ BEGIN
         [MeetingID] [int] NOT NULL,
 
         Deleted bit null,
-        VersionAutoID int null,
+        VersionAutoID bigint null,
+
         TSCreateDate datetime null,
         TSCreateUser [nvarchar](80) NOT NULL,
         TSModifyDate datetime null,
@@ -349,7 +143,8 @@ BEGIN
         [DemonstrationOnly] bit NULL,
 
         Deleted bit null,
-        VersionAutoID int null,
+        VersionAutoID bigint null,
+
         TSCreateDate datetime null,
         TSCreateUser [nvarchar](80) NOT NULL,
         TSModifyDate datetime null,
@@ -407,7 +202,8 @@ BEGIN
         [Name] [nvarchar](200) NULL,
 
         Deleted bit null,
-        VersionAutoID int null,
+        VersionAutoID bigint null,
+
         TSCreateDate datetime null,
         TSCreateUser [nvarchar](80) NOT NULL,
         TSModifyDate datetime null,
